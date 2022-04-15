8c918bd2cfd623469dc3916c9f7140f7c74a2df6
surawatthana <surawatthana@hotmail.com>
2022-04-15 15:29:41 +0700

    Implementing PassingCars

--- src/test/java/com/kodcha/a05/prefixsum/PassingCars.java
+++ src/test/java/com/kodcha/a05/prefixsum/PassingCars.java
@@ -9,24 +9,21 @@ class PassingCars {
 	@Test
 	void test() {
 		assertEquals(solution(new int[] { 0, 1, 0, 1, 1 }), 5);
-
 	}
 
 	public int solution(int[] A) {
-		int countZero = 0;
-		int pair = 0;
-		int counter = 0;
-		for (int i = A.length - 1; i >= 0; i--) {
-			counter++;
-			if (A[i] == 0) {
-				countZero++;
-				pair += counter - countZero;
-				if (pair > 1000000000) {
-					return -1;
-				}
+		int eastboundCars = 0;
+		int passingCars = 0;
+
+		for (int a : A) {
+			if (a == 0) {
+				eastboundCars++;
+			} else {
+				passingCars += eastboundCars;
 			}
 		}
-		return pair;
+
+		return Math.abs(passingCars) > 1_000_000_000 ? -1 : passingCars;
 	}
 
 }
