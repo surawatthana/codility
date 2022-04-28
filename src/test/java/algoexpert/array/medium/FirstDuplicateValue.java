94a3188590fab75a8a111c3eed9be4ab4ec5b302
Sura Watthanalamlert <surawatthana@hotmail.com>
2022-04-28 19:50:14 +0700

    Update FirstDuplicateValue.java

--- src/test/java/algoexpert/array/medium/FirstDuplicateValue.java
+++ src/test/java/algoexpert/array/medium/FirstDuplicateValue.java
@@ -3,12 +3,31 @@ package algoexpert.array.medium;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
+import java.util.*;
 class FirstDuplicateValue {
 
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
+	
+	public int myAnswer(int[] array) {
+		
+		Set<Integer> set = new HashSet<>();
+		
+		for (int i = 0; i < array.length; i++) {
+			int value = array[i];
+			
+			if (set.contains(value)) {
+				return value;
+			} else {
+				set.add(value);
+			}
+		}
+		
+		
+		
+	    return -1;
+	  }
 
 }
