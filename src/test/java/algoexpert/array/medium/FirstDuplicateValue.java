994a5f58e9078811abf1a80c25018daa9b377cf3
Sura Watthanalamlert <surawatthana@hotmail.com>
2022-04-28 19:34:43 +0700

    Create FirstDuplicateValue.java

---
+++ src/test/java/algoexpert/array/medium/FirstDuplicateValue.java
@@ -0,0 +1,14 @@
+package algoexpert.array.medium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstDuplicateValue {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
