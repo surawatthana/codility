3774aa7b898d1f4b40b395012703959b0d2c95c9
Sura Watthanalamlert <surawatthana@hotmail.com>
2022-04-30 17:18:51 +0700

    Update LongestPalindromicSubString.java

--- src/test/java/algoexpert/string/medium/LongestPalindromicSubString.java
+++ src/test/java/algoexpert/string/medium/LongestPalindromicSubString.java
@@ -22,49 +22,45 @@ class LongestPalindromicSubString {
 	}
 	
 	public static String mySolution(String str) {
-	    
-		// populate the map
-		Map<Character, Integer[]> map = new HashMap<>();
-	    for(int i = 0; i<str.length(); i++) {
-	    	Character character = str.charAt(i);
-	    	
-	    	if (map.get(character) == null) {
-	    		map.put(character, new Integer[] {i,i});
-	    	} else {
-	    		Integer[] range = map.get(character);
-	    		range[1] = i;
-	    		map.put(character, range);
-	    	}
-	    }
-	    
-	    // identify the longest range
-	    String longestPalindromicSubString = "";
-	    
-	    for (Map.Entry<Character, Integer[]> entry : map.entrySet()) {
-	    	String subString = str.substring(entry.getValue()[0] +entry.getValue()[1] + 1);
-	    	
-	    	boolean isPalindromic = true;
-	    	int i = 0;
-	    	while (i < subString.length() / 2) {
-	    		char beginChar = subString.charAt(i);
-	    		char endChar   = subString.charAt(subString.length() - i - 1);
-	    		
-	    		if (beginChar != endChar) {
-	    			isPalindromic = false;
-	    			break;
-	    		}
-	    	}
-	    	
-	    	if (isPalindromic) {
-	    		int lengthCurrentPalindromicSubString = longestPalindromicSubString.length();
-	    		int lengthNewPalindromicSubString =  subString.length();
-	    		if ( lengthNewPalindromicSubString > lengthCurrentPalindromicSubString ) {
-	    			longestPalindromicSubString = subString;
-	    		}
-	    	}
-	    }
-	    
-	    return longestPalindromicSubString;
-	  }
+		
+		if (str.length() == 1) {
+			return str;
+		}
+
+		String palindromicString = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char charStart = str.charAt(i);
+
+			for (int j = i + 1; j < str.length(); j++) {
+				char charEnd = str.charAt(j);
+				if (charStart == charEnd) {
+					String subString = str.substring(i, j + 1);
+					if (isPalindromicString(subString)) {
+						if (subString.length() > palindromicString.length()) {
+							palindromicString = subString;
+						}
+					}
+				}
+			}
+		}
+		return palindromicString;
+	}
+
+	private static boolean isPalindromicString(String subString) {
+
+		int i = 0;
+		while (i < subString.length() / 2) {
+			char beginChar = subString.charAt(i);
+			char endChar = subString.charAt(subString.length() - i - 1);
+
+			if (beginChar != endChar) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+
+	}
 
 }
